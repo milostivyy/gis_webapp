a27786f248d5f0af059689084cbd3b5ef5d1a7ae
Mahima <62246919+iammaximus@users.noreply.github.com>
2022-06-14 21:34:23 +0530

    added

--- src/index.js
+++ src/index.js
@@ -8,21 +8,30 @@ import {
   Marker
 } from "react-google-maps";
 
+const positions = [
+  {lat: 18.975, lng: 72.825833},
+  {lat: 18.70, lng: 72.8258},
+  {lat: 17.70, lng: 72.1},
+  {lat: 18.70, lng: 72.72}
+];
+
 const MyMapComponent = compose(
   withProps({
     googleMapURL:
       "https://maps.googleapis.com/maps/api/js?key=AIzaSyDj26Nvzuw98GSnNTBOsIIRpHs1sdney5Q&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
+    mapElement: <div style={{ height: `150%` }} />
   }),
   withScriptjs,
   withGoogleMap
-)(props => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: 19.7515, lng: 75.7139 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: 18.975, lng: 72.825833 }} />
-    )}
+)
+(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{lat: 18.975, lng: 72.825833}}>
+     {/* <Marker position={{ lat: 18.975, lng: 72.825833 }} /> */}
+      {positions.map((item, index) => 
+        <Marker name="Dolores park" position={item} key={index} />
+      )}
   </GoogleMap>
 ));
 
